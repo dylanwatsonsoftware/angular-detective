62aa8574eb9ada0c114babc8701a8936aa8e122d
Dylan Watson <lotsabackscatter@gmail.com>
2020-11-07 22:24:27 +0800

    Show module heirarchy

--- cli.js
+++ cli.js
@@ -3,7 +3,10 @@ const detective = require("./index");
 const ora = require("ora");
 const chalk = require("chalk");
 const fs = require("fs");
+const path = require("path");
 const printTree = require("print-tree");
+const detectiveTypescript = require("detective-typescript");
+const glob = require("glob");
 
 const spinner = ora();
 
@@ -30,32 +33,111 @@ function buildTree(filename, deps, getChild = (name) => ({ name })) {
   };
 }
 
-async function main() {
-  spinner.start().info(`Finding dependencies of ${filename}`);
+function getDepsFromModule(filename) {
+  spinner.info(`Finding dependencies of ${filename}`).start();
   const src = fs.readFileSync(filename, "utf8");
-
-  // Find all angular components below directory (component.html)
-  // Find associated ts component file (by convention)
-  // Find selector for component
-  // Build up depedency-tree of all html files that reference that selector
-
-  const moduleDeps = detective(src);
-  const moduleTree = buildTree(filename, moduleDeps);
-  printTree(
-    moduleTree,
-    (node) => node.name,
-    (node) => node.children
-  );
-
-  //   const deps = detective.getDependenciesFromHtml(src);
-  //   const depTree = buildTree(filename, deps);
-
-  spinner.succeed("Done!");
-
-  //   printTree(
-  //     depTree,
-  //     (node) => node.name,
-  //     (node) => node.children
-  //   );
+  const moduleDeps = detectiveTypescript(src);
+  return moduleDeps.filter((dep) => dep.startsWith("."));
 }
+
+function getDepsForComponent(parent, name) {
+  const dir = path.dirname(parent);
+  const fullPath = path.resolve(dir, `${name}.html`);
+  spinner.start(`Finding dependencies of component ${getFilename(name)}`);
+  const componentSrc = fs.readFileSync(fullPath, "utf8");
+  const children = detective.getDependenciesFromHtml(componentSrc);
+  spinner.succeed(
+    `Found ${children.length} dependencies of ${getFilename(name)}`
+  );
+  return {
+    name: getFilename(name),
+    children: children.map((name) => ({ name })),
+  };
+}
+
+function getDepsForModule(parent, name) {
+  const dir = path.dirname(parent);
+  const fullPath = path.resolve(dir, `${name}.ts`);
+  spinner.start(`Finding dependencies of module ${getFilename(name)}`);
+  const children = getDepsFromModule(fullPath).filter((a) =>
+    a.includes("module")
+  );
+  spinner.succeed(
+    `Found ${children.length} dependencies of ${getFilename(name)}`
+  );
+  return {
+    name: getFilename(name),
+    children: children
+      /*.filter(child => child.endsWith('.module'))*/
+      .map((child) => getDepsForModule(fullPath, child)),
+  };
+}
+
+function globp(pattern, options) {
+  return new Promise((res, rej) =>
+    glob(pattern, options, (error, files) => {
+      if (error) return rej(error);
+      res(files);
+    })
+  );
+}
+
+async function main() {
+  try {
+    // Find all angular components below directory (component.html)
+    // Find associated ts component file (by convention)
+    // Find selector for component
+    // Build up depedency-tree of all html files that reference that selector
+
+    const rootDir = path.dirname(filename);
+
+    const modules = (await globp(rootDir + "/**/*.module.ts", {})).map(
+      (module) =>
+        "./" + path.relative(path.dirname(filename), module).replace(".ts", "")
+    );
+
+    const moduleDeps = getDepsFromModule(filename);
+    const modulesFromDeps = moduleDeps.filter((dep) => dep.endsWith(".module"));
+
+    console.log(modules);
+    console.log(modulesFromDeps);
+
+    const allModules = [...new Set([...modulesFromDeps, ...modules])];
+    const moduleTree = buildTree(filename, allModules, (name) => {
+      return getDepsForModule(filename, name);
+    });
+
+    printTree(
+      moduleTree,
+      (node) => node.name,
+      (node) => node.children
+    );
+
+    const components = moduleDeps.filter((dep) => dep.endsWith(".component"));
+    const moduleComponentTree = buildTree(filename, components, (name) => {
+      return getDepsForComponent(filename, name);
+    });
+
+    printTree(
+      moduleComponentTree,
+      (node) => node.name,
+      (node) => node.children
+    );
+
+    //   const deps = detective.getDependenciesFromHtml(src);
+    //   const depTree = buildTree(filename, deps);
+
+    spinner.succeed("Done!");
+
+    //   printTree(
+    //     depTree,
+    //     (node) => node.name,
+    //     (node) => node.children
+    //   );
+  } catch (e) {
+    console.error(e);
+    spinner.fail(e.message);
+  }
+}
+
 main();
