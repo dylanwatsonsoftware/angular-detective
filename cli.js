f2183a437f26d494f0c6a1e14925174672d64c6c
Dylan Watson <lotsabackscatter@gmail.com>
2020-11-07 20:33:24 +0800

    Print deps as tree

--- cli.js
+++ cli.js
@@ -1,13 +1,60 @@
 #!/usr/bin/env node
 const detective = require("./index");
 const ora = require("ora");
+const chalk = require("chalk");
+const fs = require("fs");
+const printTree = require("print-tree");
 
-const filename = process.argv[3] || ".";
+const spinner = ora();
 
-const spinner = ora(`Finding dependencies of ${filename}`).start();
+const filename = process.argv[2];
+
+if (!filename) {
+  spinner.fail(
+    `${chalk.red("You must provide a file to search dependencies for:")}`
+  );
+  spinner.info(`e.g: ${chalk.gray("npx detective-angular app.component.ts")}`);
+  console.log(process.argv);
+  return;
+}
 
 setTimeout(() => {
-  spinner.color = "yellow";
   spinner.text = "Loading rainbows";
-  spinner.succeed("Doneski");
 }, 1000);
+
+// Find all angular components below directory (component.html)
+// Find associated ts component file (by convention)
+// Find selector for component
+// Build up depedency-tree of all html files that reference that selector
+
+function printDeps(deps) {
+  deps.forEach((dep) => {
+    spinner.info(`${chalk.green(dep)}`);
+  });
+}
+
+function getFilename(filename) {
+  const parts = filename.split("/");
+  return parts[parts.length - 1];
+}
+
+async function main() {
+  spinner.start(`Finding dependencies of ${filename}`);
+  const src = fs.readFileSync(filename, "utf8");
+  const deps = detective.getDependenciesFromHtml(src);
+  const depTree = {
+    name: getFilename(filename),
+    children: deps.map((name) => ({
+      name,
+    })),
+  };
+
+  console.log("\n");
+  printTree(
+    depTree,
+    (node) => node.name,
+    (node) => node.children
+  );
+  spinner.succeed("Done!");
+}
+main();
--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-// Find all angular components below directory (component.html)
-// Find associated ts component file (by convention)
-// Find selector for component
-// Build up depedency-tree of all html files that reference that selector
-
 "use strict";
 
 const Parser = require("@typescript-eslint/typescript-estree");
